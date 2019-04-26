dc27342df4925f424e214873c04598e1cc2650a0
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2019-04-26 18:35:41 +0300

    added interface export

--- src/cpp-function-headers.ts
+++ src/cpp-function-headers.ts
@@ -1,68 +1,74 @@
-interface FunctionParameter {
-  name: string;
-  type: string;
-  dimensions: number;
+export interface FunctionParameter {
+  name: string
+  type: string
+  dimensions: number
 }
 
-interface FunctionHeader {
-  functionName: string;
-  returnType: string;
-  parameters: FunctionParameter[];
+export interface FunctionHeader {
+  functionName: string
+  returnType: string
+  parameters: FunctionParameter[]
 }
 
 /*
-* Given a function name, extract the signature (return type, parameters).
-* If there is no function matching with the provided name, null is returned.
-*/
-export function ExtractFunctionHeader(functionName: string, cppSource: string): FunctionHeader | null {
-  let regex = `([A-Za-z0-9_]+)[\\n\\r\\s]*${functionName}[\\n\\r\\s]*\((.|\\n|\\s|\\r)*?\)[\\n\\r\\s]*{`;
-  let match = cppSource.match(regex);
+ * Given a function name, extract the signature (return type, parameters).
+ * If there is no function matching with the provided name, null is returned.
+ */
+export function ExtractFunctionHeader(
+  functionName: string,
+  cppSource: string
+): FunctionHeader | null {
+  let regex = `([A-Za-z0-9_]+)[\\n\\r\\s]*${functionName}[\\n\\r\\s]*\((.|\\n|\\s|\\r)*?\)[\\n\\r\\s]*{`
+  let match = cppSource.match(regex)
   if (!match) {
-    return null;
+    return null
   }
-  let paramMatches = match[2].replace('(', '').replace(')', '').split(',');
-  let parameters = [];
+  let paramMatches = match[2]
+    .replace('(', '')
+    .replace(')', '')
+    .split(',')
+  let parameters = []
   for (let paramMatch of paramMatches) {
-    let paramType = '';
-    let paramName = '';
-    let foundParamType = false;
-    let foundParamName = false;
+    let paramType = ''
+    let paramName = ''
+    let foundParamType = false
+    let foundParamName = false
     for (let chr of paramMatch) {
-      if ([' ','\n','\r','\t'].indexOf(chr) > -1) {
+      if ([' ', '\n', '\r', '\t'].indexOf(chr) > -1) {
         if (paramType.length > 0) {
-          foundParamType = true;
+          foundParamType = true
         }
-        continue;
+        continue
       }
       if (!foundParamType) {
-       paramType += chr;
+        paramType += chr
       } else if (!foundParamName && chr === '[') {
-        foundParamName = true;
-        paramType += chr;
+        foundParamName = true
+        paramType += chr
       } else if (!foundParamName) {
-        paramName += chr;
+        paramName += chr
       } else if (foundParamName) {
         if (chr === '[' || chr === ']' || (chr >= '0' && chr <= '9')) {
-         paramType += chr;
+          paramType += chr
         }
       }
     }
 
     // filter empty strings
     if (!paramName) {
-      continue;
+      continue
     }
 
     parameters.push({
       name: paramName,
       type: paramType,
       dimensions: paramType.split('').filter(x => x === '[').length
-    });
+    })
   }
 
   return {
     functionName: functionName,
     parameters: parameters,
     returnType: match[1] ? match[1] : 'void'
-  };
+  }
 }
